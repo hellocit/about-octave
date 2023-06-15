5edb04fa8afe3e1b2e5de8c871c0ad1c02193088
hellocit <bestoosaka@gmail.com>
2023-06-15 12:05:18 +0900

    add 9 pra3

--- 9/pra3.m
+++ 9/pra3.m
@@ -1,42 +1,53 @@
 clear();
 pkg load control
 
+m = 0.2;
+l = 0.3;
+M = 0.3;
+g = 9.8;
+J = 1/12*m*l^2;
+
+a = (M+m)*(m*l^2+J)-m^2*l^2;
+A = [0, 0, 1, 0; 0, 0, 0, 1;]
+
+
+
+
 A = [0 1; -5 -6];
 B = [0; 1];
 C = [0 1];
 
 Mo = [C; C*A];
-det(Mo);
 
-Aa = A'
-Ba = C'
-Ca = B'
-D = 0
+Aa =
+Ba = 
+Ca =
+D = 
 
 sys = ss(Aa, Ba, Ca, D);
-P = [-3, -4];
-L = place(sys, P)';
+P = ;
+L = place(sys, P);
 
 u = 1;
 x = [1; 1];
 xh = [0;0];
 yh = 0;
-dt = 0.1;
 t = 0:dt:10;
 x = [0;0];
+dt = 0.1;
 i = 0;
 
 for n = t
 	i = i + 1;
-	dx = A * x + B * u;
-	x = x + dx * dt;
-	y = C * x;
+	dx = 
+	x = 
+	y =
 	x1(i) = x(1);
 	x2(i) = x(2);
 
-	dxh = A * xh + B * u - L * (yh - y);
-	xh = xh + dxh * dt;
-	yh = C * xh;
+	dxh =
+	xh = 
+	yh = 
 	xh1(i) = xh(1);
 	xh2(i) = xh(2);
 endfor
